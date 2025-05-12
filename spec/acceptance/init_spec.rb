fbf8887bb3769bc33c3d27b3e21722d9930b9340
Joshua Hoblitt <josh@hoblitt.com>
2025-05-12 16:24:50 -0700

    rm s3daemon::instance::s3_endpoint_url param
    This param creates an env var in the container and the hardcoded
    behavior prevents the env var from being renamed by the implementation.

--- spec/acceptance/init_spec.rb
+++ spec/acceptance/init_spec.rb
@@ -19,6 +19,7 @@ describe 's3daemon' do
         it { is_expected.to contain 'AWS_ACCESS_KEY_ID=access_key_id' }
         it { is_expected.to contain 'AWS_SECRET_ACCESS_KEY=secret_access_key' }
         it { is_expected.to contain 'QUUX=corge' }
+        it { is_expected.to contain 'FOO=bar' }
       end
 
       describe docker_container('systemd-s3daemon-foo') do
@@ -49,9 +50,8 @@ describe 's3daemon' do
         it { is_expected.to contain 'S3_ENDPOINT_URL=https://s3.bar.example.com' }
         it { is_expected.to contain 'AWS_ACCESS_KEY_ID=access_key_id' }
         it { is_expected.to contain 'AWS_SECRET_ACCESS_KEY=secret_access_key' }
-        it { is_expected.to contain 'FOO=bar' }
-        it { is_expected.to contain 'BAZ=qux' }
         it { is_expected.to contain 'QUUX=corge' }
+        it { is_expected.to contain 'BAZ=qux' }
       end
 
       describe docker_container('systemd-s3daemon-bar') do
